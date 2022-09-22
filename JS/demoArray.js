88219749d7004284b93269b833abe3562ea04a61
quangnamfpt <nameluong@gmail.com>
2022-09-22 16:29:11 +0700

    first commit

---
+++ JS/demoArray.js
@@ -0,0 +1,45 @@
+// console.log('Hello bro');
+
+// let a = {
+//     name: 'Nam',
+//     age: 18
+// }
+
+// console.log(a);
+// console.log(a.name);
+
+// //arr = [item1, item2,...]
+// let array = ['MU', 'Chelsea', 'Liverful'];
+// console.log(typeof array, array);
+// console.log(array[0], array[2]);
+
+// let family = [
+//     {name: 'Nam',age: 18},
+//     {name: 'Ok1',age: 19},
+//     {name: 'ok2',age: 12},
+// ]
+// console.log(family);
+
+
+// let sum = (a, b) => {
+//     return a + b;
+// }
+
+// console.log(sum(9, 10));
+
+
+// let obj = {
+//     name: 'Nam',
+//     ID: 'HE141346',
+//     // getName: function(){
+//     //     return this.name;
+//     //     //return obj.name;
+//     // }
+//     getName: getName2 = () => {
+//         return this.name;
+//     }
+// }
+// console.log(obj.getName2());
+
+
+
---
+++ JS/demoObject.js
@@ -0,0 +1,17 @@
+console.log('Hello world');
+
+//key: value
+let obj = {
+    name: 'Quang Nam',
+    age: 18,
+    email: 'nameluong@gmail.com'
+};
+
+JSON
+"{name: 'Name', address: 'Hai Duong'}"
+
+obj.name='Nam Vjp'
+
+// console.log(`My name is ${obj.name}`);
+// console.log(`I'm ${obj.age} year old`);
+console.log(`My name is`, obj['name'], obj);
